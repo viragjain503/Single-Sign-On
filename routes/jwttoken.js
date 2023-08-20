c71a3c3f7233bbf1a37e9cda4393157c117c6968
viragjain503 <viragjain503@gmail.com>
2023-08-20 17:42:09 +0530

    Final commit ..complete sso and login work flow working

--- routes/jwttoken.js
+++ routes/jwttoken.js
@@ -1,45 +1,87 @@
-var express = require('express');
-var router = express.Router();
-var jwt = require('jsonwebtoken');
-const fs = require("fs");
+const express = require('express');
+const router = express.Router();
+const jwt = require('jsonwebtoken');
+const fs = require('fs').promises; // Import fs with promises
 
-/* CREATE JWT token first time*/
-router.post('/signdetails', function(req, res, next) {
+async function generateObject(body) {
+  try {
+    const jsonString = await fs.readFile('./db/clients.json', 'utf8');
+    const clientsFile = JSON.parse(jsonString);
+    const clients = clientsFile.clients;
+    const client = clients.find(client => client.clientId === body.clientId);
+    return {
+      clientId: body.clientId,
+      username: body.username,
+      permissions: client.permissions,
+    };
+  } catch (err) {
+    console.log('File read failed for users:', err);
+    throw err; // Rethrow the error to handle it further up
+  }
+}
 
-  fs.readFile("./db/customer.json", "utf8", (err, jsonString) => {
-    if (err) {
-      console.log("File read failed:", err);
-      return;
-    }
+async function requiredUtils(body) {
+  try {
+    const jsonString = await fs.readFile('./db/clients.json', 'utf8');
+    const clientsFile = JSON.parse(jsonString);
+    const clients = clientsFile.clients;
+    const client = clients.find(client => client.clientId === body.clientId);
+    return {
+      clientSecret: client.clientSecret,
+      redirectPage: client.redirectPage,
+      expiresIn: client.expiresIn,
+    };
+  } catch (err) {
+    console.log('File read failed for users:', err);
+    throw err; // Rethrow the error to handle it further up
+  }
+}
+
+/* CREATE JWT token first time */
+router.post('/signdetails', async (req, res, next) => {
+  try {
+    const jsonString = await fs.readFile('./db/customer.json', 'utf8');
     const customer = JSON.parse(jsonString);
-    //check if client exists or not..
-    var users = customer.users;
-    if(users.includes(req.body.username)){
-      var token = jwt.sign({ username : req.body.username }, 'mySecretKey',{ expiresIn: '30s' });
-      res.json({
-        token: token
-      });
-    }else{
-      res.status(404).send("Incorrect Credentials");
-    }
-  });
 
+    // Check if client exists or not
+    const users = customer.users;
+    if (users.includes(req.body.username)) {
+      const jwtObject = await generateObject(req.body);
+      const utilsObject = await requiredUtils(req.body);
+      
+      const token = jwt.sign({ jwtObject: jwtObject }, utilsObject.clientSecret, {
+        expiresIn: '15m',
+      });
+
+      res.json({
+        token: token,
+        redirectPage: utilsObject.redirectPage,
+        permissions: jwtObject.permissions,
+      });
+    } else {
+      res.status(404).send('Incorrect Credentials');
+    }
+  } catch (error) {
+    console.log('Error:', error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
-/* check the jwt token for username*/
-router.post('/checkjwt', function(req, res, next) {
+/* check the jwt token for username */
+router.post('/checkjwt', (req, res, next) => {
   try {
-    var decodedtoken = jwt.verify(req.headers.token, 'mySecretKey');
+    const decodedtoken = jwt.verify(req.headers.token, 'mySecretKey');
+
+    if (decodedtoken && decodedtoken.jwtObject.username) {
+      res.status(200).json({
+        message: 'Correct token buddy',
+      });
+    } else {
+      res.status(400).send('Wrong token details buddy');
+    }
   } catch (error) {
-    console.log(error);
-  }
- 
-  if(decodedtoken && decodedtoken.username == "viragjain503"){
-    res.status(200).json({
-      message : "Correct token buddy"
-    });
-  }else{
-    res.status(400).send("Wrong token details buddy");
+    console.log('Error:', error);
+    res.status(500).send('Internal Server Error');
   }
 });
 
